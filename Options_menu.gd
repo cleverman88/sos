1f4302c38d08fefbd27c05ca734fa4b6d0334edb
GitKapster <kapi251003@gmail.com>
2024-01-27 00:40:32 +0000

    hmm?

--- Options_menu.gd
+++ Options_menu.gd
@@ -3,4 +3,8 @@ extends Button
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+
+func _on_volume_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Volume_Menu.tscn")
---
+++ Scenes/Volume_Menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var master_bus = AudioServer.get_bus_index("Master")
+@export var Playing = false
+
+var audio_stream2d = get_tree().get_nodes_in_group("audio")[0]
+
+func _ready():
+	AudioServer.set_bus_volume_db(master_bus, 0)
+	audio_stream2d.stop()
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+
+	if value >= 1 and not Playing:
+		Playing = true
+		audio_stream2d.play()
+	elif value == 0:
+		AudioServer.set_bus_mute(master_bus, true)
+		Playing = false
+		audio_stream2d.stop()
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+		audio_stream2d.play()
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
