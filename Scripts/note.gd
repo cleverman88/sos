e4fcc67807a86136f12b196fe5c0cb31388931a2
cleverman88 <sohaibkarous@gmail.com>
2024-01-27 02:31:03 +0000

    kinda works???

--- Scripts/note.gd
+++ Scripts/note.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 # Define the velocity (pixels per second). Adjust x for horizontal speed.
 var velocity = Vector2(100, 0)  # Move to the right at 100 pixels per second.
-
+var done_out = false
+	
 func _ready():
 	# If you need to initialize something
 	pass
--- Scripts/spawn.gd
+++ Scripts/spawn.gd
@@ -4,10 +4,11 @@ var node_scene = preload("res://Scenes/note.tscn")
 
 @onready var conductor = $"../conductor"
 
+
 var spawned_nodes = [] 
 
 var INSIDE = false
-
+	
 
 func _ready():
 	conductor.beat.connect(_on_beat)
@@ -22,26 +23,29 @@ func _input(event):
 	if event.is_action_pressed("ui_right"):  # Check if the right arrow key was pressed
 		if len(spawned_nodes) > 0:  # Check if there's a spawned node
 			if INSIDE:
-				hit()
+				INSIDE = false
+				hit(spawned_nodes[-1])
 			else:
-				missed()
+				missed(spawned_nodes[-1])
 			spawned_nodes.pop_front().queue_free()  # Remove the spawned node
 
 
 func _on_mic_stand_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
 	INSIDE = true
-	
-	
-func _on_mic_stand_area_shape_exited(area_rid, area, area_shape_index, local_shape_index):
-	# If the node is not hit it should just vanish 
-	INSIDE = false # Replace with function body.
-	if len(spawned_nodes) > 0:
-		missed()
-		spawned_nodes.pop_front().queue_free()  # Remove the spawned node
 
 
-func missed():
+func missed(node):
 	print("MISSED")
 	
-func hit():
+func hit(node):
+
 	print("HIT")
+
+
+
+func _on_missed_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	INSIDE = false # Replace with function body.
+	if len(spawned_nodes) > 0:
+		missed(spawned_nodes[-1])
+		spawned_nodes.pop_front().queue_free()  # Remove the spawned node
+		
