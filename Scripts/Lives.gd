c206e0c501171ef5b5625ef86b5ec02405d7c6bf
cleverman88 <sohaibkarous@gmail.com>
2024-01-27 12:57:12 +0000

    king bach

--- Scripts/Lives.gd
+++ Scripts/Lives.gd
@@ -1,6 +1,14 @@
-extends Label
+extends Node2D
 
-var total_lives = 3
+var total_lives = 5
+var prev_lives = total_lives
+
+@onready var good = $Good
+@onready var medium = $Medium
+@onready var medium_animation = $MediumBoom/AnimationPlayer
+@onready var bad_animation = $BadBoom/AnimationPlayer
+@onready var bad = $Bad
+@onready var boom = $boom
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +16,33 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	text = str(total_lives)+" Lives"
+	if total_lives >= 4:
+		good.visible = true
+		medium.visible = false
+		bad.visible = false
+		if total_lives != prev_lives:
+			prev_lives = total_lives
+			boom.play()
+			medium_animation.play("expload")
+		return
+	if total_lives >= 2:
+		if total_lives != prev_lives:
+			prev_lives = total_lives
+			boom.play()
+			bad_animation.play("expload")
+		good.visible = false
+		medium.visible = true
+		bad.visible = false
+		if total_lives != prev_lives:
+			prev_lives = total_lives
+			boom.play()
+			bad_animation.play("expload")
+		return
+	else:
+		if total_lives != prev_lives:
+			prev_lives = total_lives
+			boom.play()
+			bad_animation.play("expload")
+		good.visible = false
+		medium.visible = false
+		bad.visible = true
