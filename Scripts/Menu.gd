aa28a2e2664a11fcba579961447922d270f3e6fc
Kapi <kapi251003@gmail.com>
2024-01-28 15:29:25 +0000

    HowToPlayScreen

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -12,7 +12,7 @@ func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 
 func _on_how_to_play_pressed():
-	pass
+	get_tree().change_scene_to_file("res://Scenes/how_to_play.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
