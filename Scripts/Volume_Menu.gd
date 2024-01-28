d33785998e9e851085c9f539688a6ca263b8a5c6
Kapi <kapi251003@gmail.com>
2024-01-28 15:35:30 +0000

    Fixed Music In Menus

--- Scripts/Volume_Menu.gd
+++ Scripts/Volume_Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 var master_bus = AudioServer.get_bus_index("Master")
-@export var Playing = false
+var Playing = false
 var audio_stream2d: AudioStreamPlayer2D
 
 
@@ -9,6 +9,7 @@ func _ready():
 	AudioServer.set_bus_volume_db(master_bus, 0)
 	audio_stream2d = $AudioStreamPlayer2D
 	audio_stream2d.stop()
+	
 
 func _on_h_slider_value_changed(value):
 	AudioServer.set_bus_volume_db(master_bus, value)
@@ -19,10 +20,8 @@ func _on_h_slider_value_changed(value):
 	elif value == 0:
 		AudioServer.set_bus_mute(master_bus, true)
 		Playing = false
-		audio_stream2d.stop()
-	else:
+	elif Playing:
 		AudioServer.set_bus_mute(master_bus, false)
-		audio_stream2d.play()
 
 
 func _on_back_pressed():
